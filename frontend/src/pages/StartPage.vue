24e71dba43d8d258ebdb57352038aa41c8fb84fc
Udo Oehmig <udo.oehmig@gmail.com>
2023-09-22 11:29:11 +0200

    Slight enhancment to edit / confirm on start page

--- frontend/src/pages/StartPage.vue
+++ frontend/src/pages/StartPage.vue
@@ -3,14 +3,15 @@
     <div>
       <q-card flat class="bg-grey-2 q-mt-lg q-ma-md">
         <q-card-section>
-          <div class="text-h6 text-weight-bold text-center">Wieviele Kinder bekommen heute Essen?</div>
+          <div v-if="meals_confirmed" class="text-h6 text-weight-bold text-center">Heute wurde bereits das Essen bestellt für </div>
+          <div v-else class="text-h6 text-weight-bold text-center">Wieviele Kinder bekommen heute Essen?</div>
         </q-card-section>
 
         <q-card-section>
           <q-card flat class="bg-blue q-ma-md">
             <div class="row q-pt-lg">
               <div class="col">
-                <div class="text-h1 text-weight-bolder text-center text-white">24</div>
+                <div class="text-h1 text-weight-bolder text-center text-white">{{ count }}</div>
               </div>
             </div> 
             <div class="row q-pb-lg">
@@ -20,13 +21,27 @@
             </div>
           </q-card>
         </q-card-section>
-
-        <q-card-section>
+        <q-card-section v-if="meals_confirmed">
           <div class="column q-pa-sm items-center">
-            <q-btn to="/start" class="btn-fixed-width" color="negative" text-color="white" label="Ändern" />
+            <div class="q-gutter-md">
+              <q-btn class="btn-small-fixed-width text-h6" disabled color="negative" text-color="white" label="-"/>
+              <q-btn class="btn-small-fixed-width text-h6" disabled color="negative" text-color="white" label="+"/>          
+            </div>
           </div>
           <div class="column q-pa-sm items-center">
-            <q-btn to="/start" class="btn-fixed-width" color="positive" text-color="white" label="Bestätigen" />
+            <q-btn class="btn-fixed-width" color="info" text-color="white" label="Korrigieren" @click="meals_confirmed=false"/>
+          </div>
+        </q-card-section>
+        <q-card-section v-else>
+          <div class="column q-pa-sm items-center">
+            <div class="q-gutter-md">
+              <q-btn v-if="count > 0" class="btn-small-fixed-width text-h6" color="negative" text-color="white" label="-" @click="count--"/>
+              <q-btn v-if="count == 0" class="btn-small-fixed-width text-h6" disabled color="negative" text-color="white" label="-"/>
+              <q-btn class="btn-small-fixed-width text-h6" color="negative" text-color="white" label="+" @click="count++"/>          
+            </div>
+          </div>
+          <div class="column q-pa-sm items-center">
+            <q-btn class="btn-fixed-width" color="positive" text-color="white" label="Bestätigen" @click="confirm=true"/>
           </div>
         </q-card-section>
       </q-card>
@@ -37,7 +52,21 @@
             <q-btn to="/" class="btn-fixed-width" color="primary" text-color="white" label="Logout" />
           </div>
         </q-card-section>
-      </q-card>      
+      </q-card> 
+
+      <q-dialog v-model="confirm" persistent>
+        <q-card>
+          <q-card-section class="row items-center">
+            <q-avatar icon="lunch_dining" color="primary" text-color="white" />
+            <span class="q-ml-sm">Hiermit wird eine Bestellung für {{ count }} Essen aufgegeben.</span>
+          </q-card-section>
+
+          <q-card-actions align="right">
+            <q-btn flat label="Abbrechen" color="primary" v-close-popup />
+            <q-btn flat label="Bestätigen" color="primary" @click="meals_confirmed=true" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>     
     </div>
   </q-page>
 </template>
@@ -45,15 +74,25 @@
 <style lang="sass" scoped>
 .btn-fixed-width
   width: 200px
+.btn-small-fixed-width
+  width: 90px
 </style>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: "StartPage",
   setup() {
-    
+    return {
+      confirm: ref(false),
+      meals_confirmed: ref(false)
+    }
+  },
+  data() {
+    return {
+      count: 24,
+    }
   }
 })
 </script>
