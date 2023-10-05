720e0e82c4f695989ca9b4d9a795ba43bca7c924
Udo Oehmig <udo.oehmig@gmail.com>
2023-10-05 09:17:30 +0200

    Create users table

---
+++ hasura/migrations/default/1696490221561_create_table_public_users/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."users" ("id" serial NOT NULL, "email" text NOT NULL, "vorname" text NOT NULL, "nachname" text NOT NULL, "rolle" text NOT NULL, "password" text NOT NULL, PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("email"));COMMENT ON TABLE "public"."users" IS E'Users table';
